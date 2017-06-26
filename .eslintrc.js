6a5fe9ccae539b5feadc82fe84294a344350996b
shnider <shnider7860@gmail.com>
2017-06-26 18:59:45 +0300

    feat/intsall husky, add prepush, disable some Eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,11 @@
 module.exports = {
   extends: 'airbnb',
   plugins: ['react', 'jsx-a11y', 'import'],
+  env: {
+    browser: true,
+    jest: true,
+  },
+  rules: {
+    'react/prop-types': 0,
+  },
 };
---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Complexes from './Complexes/List';
+import Complex from './Complexes/Show';
+import Header from './Header';
+import Footer from './Footer';
+import './App.css';
+
+const App = () =>
+  (<Router>
+    <div>
+      <Header />
+      <Route exact path="/" component={Complexes} />
+      <Route path="/complex" component={Complex} />
+      <Footer />
+    </div>
+  </Router>);
+
+export default App;
