d0fa30dc82eff57a87db3f77f74f264603f4b8b5
shnider <shnider7860@gmail.com>
2017-06-30 11:52:01 +0300

    fix: in api.js URL const name, string  interpolation replaced by concatenating

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/prefer-default-export*/
 
-const API_URL_HOST = 'https://api.jqestate.ru';
+const API_URL_BASE = 'https://api.jqestate.ru';
 
 export function get(relativePath) {
-  return fetch(`${API_URL_HOST}${relativePath}`).then(resp => resp.json());
+  return fetch(API_URL_BASE + relativePath).then(resp => resp.json());
 }
