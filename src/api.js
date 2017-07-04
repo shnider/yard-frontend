8ac81fd5290b53b0bf4a3df893262cada3e257fb
shnider <shnider7860@gmail.com>
2017-07-04 21:13:52 +0300

    fix: naming argument get

--- src/api.js
+++ src/api.js
@@ -3,6 +3,6 @@
 
 const API_URL_BASE: string = 'https://api.jqestate.ru';
 
-export function get(relativePath: string): Promise<any> {
-  return fetch(API_URL_BASE + relativePath).then(resp => resp.json());
+export function get(resource: string): Promise<any> {
+  return fetch(API_URL_BASE + resource).then(resp => resp.json());
 }
